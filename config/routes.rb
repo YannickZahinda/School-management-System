fa96fad8f0f56fd82202b7feec703c2c7fa0b2e6
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-10-19 00:58:43 +0200

    add comments sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :students
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'home#index'
+  # get '/users/sign_out', action: :destroy, controller: 'users'
   get '/users', action: :index, controller: 'users'
   get '/dashboard', action: :index, controller: 'dashboard'
   get 'teachers', action: :index, controller: 'teachers'
+
   resources :home, only: %i[index] do
-    # resources :teachers, only: %i[index]
     resources :students, only: %i[index show create delete]
   end
 end
